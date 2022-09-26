85fa282d045a67eb2daa35b2f02de85d791e3070
nhlamuloj <nmahlabana@gmail.com>
2022-09-26 15:46:25 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
+import { BrowserRouter,Route,Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+import Navbar from './components/Navbar';
+import About from './pages/About';
+// import Footer from './components/Footer';
+import Contact from './pages/Contact';
+import Booknow from './pages/Booknow';
+// import Login from './pages/Login';
+// import Register from './pages/Register';
+import Payment from './pages/Payments';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> nhlamulo.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar/>
+        <Switch>
+          {/* <Route exact path="/" component={Login}/> */}
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/rooms/:id" component={SingleRoom} />
+          <Route path="/rooms/" component={Rooms}/>
+          <Route path="/booknow/:id" component={Booknow} />
+          <Route path='/payments' component={Payment}/>
+          {/* <Route path='/login' component={Login}/>
+          <Route path='/register' component={Register}/> */}
+          <Route component={Error}/>
+        </Switch>
+        {/* <Footer/> */}
+      </BrowserRouter>
     </div>
   );
 }
